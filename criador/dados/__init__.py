9e64ab7c4bbc24ca6bdd3245ff3c812a46055cc7
Ícaro Chagas <64924345+IcaroChagas@users.noreply.github.com>
2020-06-20 15:58:52 -0300

    Adicionando classes e pts de habilidade inicial

--- criador/dados/__init__.py
+++ criador/dados/__init__.py
@@ -1,13 +1,15 @@
+# Data for Races, Sub-races, Classes and Characteristics
+
 import random
 
+## Races
 races = ['dwarf', 'elf', 'halfling', 'half_elf', 'human', 'dragonborn', 'half_orc', 'tiefling', 'gnome']
-classes = []
 
-# list of sub-races
 
+## Sub-races
 s_races = [{'dwarf': ['mountain dwarf', 'hill dwarf']},
            {'elf': ['dark elf', 'high elf', 'wood elf']},
-           {'halfling': ['lghtfoot halfling', 'stout halfling']},
+           {'halfling': ['lightfoot halfling', 'stout halfling']},
            {'half_elf': ['half_elf']},
            {'human': ['human']},
            {'dragonborn': ['black', 'blue', 'brass', 'bronze', 'copper', 'gold', 'green', 'red', 'silver']},
@@ -15,31 +17,80 @@ s_races = [{'dwarf': ['mountain dwarf', 'hill dwarf']},
            {'gnome': ['rock gnome', 'forest gnome']},
            {'half_orc': ['half_orc']}]
 
-# equipment
-weapons = []
-armors = []
 
-# classes
+## Classes
+classes = ['Barbarian', 'Bard', 'Warlock', 'Cleric', 'Druid', 'Sorcerer', 'Fighter', 'Rogue', 'Wizard', 'Monk', 'Paladin', 'Ranger']
 
-classes = [{'fighter': ['']}]
+###Classes cheat:
+# Bárbaro = Barbarian;
+# Bardo = Bard;
+# Bruxo = Warlock;
+# Clérigo = Cleric;
+# Druida = Druid;
+# Feiticeiro = Sorcerer;
+# Guerreiro = Fighter;
+# Ladino = Rogue;
+# Mago = Wizard;
+# Monge = Monk;
+# Paladino = Paladin;
+# Patrulheiro = Ranger;
 
-# alignment
-# attributes
 
-atributes = ['Strength', 'Intelligence', 'Constitution', 'Wisdom', 'Dexterity', 'Charisma']
+"""Characteristics"""
 
-# spellbook
+## Alignment
 
-# dices
+## Abilities
+abilities = ['Strength', 'Intelligence', 'Constitution', 'Wisdom', 'Dexterity', 'Charisma']
 
-# race modifiers
+## Initial Abilities Values according to Race
+#_initial_ability_values = [[{'mountain dwarf':     {{'Strength': 2}, {'Intelligence': 0}, {'Constitution': 2}, {'Wisdom': 0}, {'Dexterity': 0}, {'Charisma': 0}}},
+#                           {'hill dwarf':         {{'Strength': 0}, {'Intelligence': 0}, {'Constitution': 2}, {'Wisdom': 1}, {'Dexterity': 0}, {'Charisma': 0}}},
+#                           {'dark elf':           {{'Strength': 0}, {'Intelligence': 0}, {'Constitution': 0}, {'Wisdom': 0}, {'Dexterity': 2}, {'Charisma': 1}}},
+#                           {'high elf':           {{'Strength': 0}, {'Intelligence': 1}, {'Constitution': 0}, {'Wisdom': 0}, {'Dexterity': 2}, {'Charisma': 0}}},
+#                           {'wood elf':           {{'Strength': 0}, {'Intelligence': 0}, {'Constitution': 0}, {'Wisdom': 1}, {'Dexterity': 2}, {'Charisma': 0}}},
+#                           {'lightfoot halfling': {{'Strength': 0}, {'Intelligence': 0}, {'Constitution': 0}, {'Wisdom': 0}, {'Dexterity': 2}, {'Charisma': 1}}},
+#                           {'stout halfling':     {{'Strength': 0}, {'Intelligence': 0}, {'Constitution': 1}, {'Wisdom': 0}, {'Dexterity': 2}, {'Charisma': 0}}},
+#                           {'half_elf':           {{'Strength': 0}, {'Intelligence': 0}, {'Constitution': 0}, {'Wisdom': 0}, {'Dexterity': 0}, {'Charisma': 2}}},  # Two other abilities (of ur choice) increased by 1.
+#                           {'human':              {{'Strength': 1}, {'Intelligence': 1}, {'Constitution': 1}, {'Wisdom': 1}, {'Dexterity': 1}, {'Charisma': 1}}},
+#                           {'dragonborn':         {{'Strength': 2}, {'Intelligence': 0}, {'Constitution': 0}, {'Wisdom': 0}, {'Dexterity': 0}, {'Charisma': 1}}},
+#                           {'tiefling':           {{'Strength': 0}, {'Intelligence': 1}, {'Constitution': 0}, {'Wisdom': 0}, {'Dexterity': 0}, {'Charisma': 2}}},
+#                           {'rock gnome':         {{'Strength': 0}, {'Intelligence': 2}, {'Constitution': 1}, {'Wisdom': 0}, {'Dexterity': 0}, {'Charisma': 0}}},
+#                           {'forest gnome':       {{'Strength': 0}, {'Intelligence': 2}, {'Constitution': 0}, {'Wisdom': 0}, {'Dexterity': 1}, {'Charisma': 0}}},
+#                           {'half_orc':           {{'Strength': 2}, {'Intelligence': 0}, {'Constitution': 1}, {'Wisdom': 0}, {'Dexterity': 0}, {'Charisma': 0}}}]]
 
+initial_ability_values = [  # [ s_race, Strength, Intelligence, Constitution, Wisdom, Dexterity, Charisma]
+                           ['mountain dwarf',      2,0,2,0,0,0],
+                           ['hill dwarf',          0,0,2,1,0,0],
+                           ['dark elf',            0,0,0,0,2,1],
+                           ['high elf',            0,1,0,0,2,0],
+                           ['wood elf',            0,0,0,1,2,0],
+                           ['lightfoot halfling',  0,0,0,0,2,1],
+                           ['stout halfling',      0,0,1,0,2,0],
+                           ['half_elf',            0,0,0,0,0,2],  # Two other abilities (of ur choice) increased by 1.
+                           ['human',               1,1,1,1,1,1],
+                           ['dragonborn',          2,0,0,0,0,1],
+                           ['tiefling',            0,1,0,0,0,2],
+                           ['rock gnome',          0,2,1,0,0,0],
+                           ['forest gnome',        0,2,0,0,1,0],
+                           ['half_orc',            2,0,1,0,0,0]]
+
+
+## Spellbook
+
+## Dices
+
+## Race modifiers
 r_modifiers = [{'dwarf': ['mountain dwarf', 'hill dwarf']},
                {'elf': ['dark elf', 'high elf', 'wood elf']},
-               {'halfling': ['lghtfoot halfling', 'stout halfling']},
+               {'halfling': ['lightfoot halfling', 'stout halfling']},
                {'half_elf': ['half_elf']},
                {'human': ['human']},
                {'dragonborn': ['black', 'blue', 'brass', 'bronze', 'copper', 'gold', 'green', 'red', 'silver']},
                {'tiefling': ['tiefling']},
                {'gnome': ['rock gnome', 'forest gnome']},
                {'half_orc': ['half_orc']}]
+
+## Equipment
+weapons = []
+armors = []
