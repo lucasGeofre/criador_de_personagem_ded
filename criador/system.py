0674ec05291cb39792d8801c7dc4fcf4d7a0c8de
IcaroChagas <icaro.chagas004@gmail.com>
2020-06-22 00:24:07 -0300

    Second test updating

--- criador/system.py
+++ criador/system.py
@@ -1,17 +1,26 @@
-from criador.classe import player
-from criador.interface import menu
-from criador.dados import *
-from criador.funcoes import *
+# System test
 
-# first-step = choosing race
+## Importing modules
+from criador.classes import *
+from criador.interface_ import menu
+from criador.data import *
+from criador.functions import *
+
+## Choosing Race
 race_choice_num = menu(races, 'Races')
 race_choice = races[race_choice_num - 1]
 
-# second-step = choosing racial trace
-s_race_choice_num = menu(sub_div(race_choice,s_races), 'Race traces')
-s_race_choice = sub_div(race_choice,s_races)[s_race_choice_num - 1]
+## Choosing Sub-race
+s_race_choice_num = menu(sub_div(race_choice), 'Race traces')
+s_race_choice = sub_div(race_choice)[s_race_choice_num-1]
 
-# applying info to player() object
-character = player(race=race_choice, s_race=s_race_choice)
-character.choose_atr()
-character.list_atr()
+## Choosing Class
+clss_choice_num = menu(classes, 'Classes')
+clss_choice = classes[clss_choice_num - 1]
+
+
+# Applying info to player() object
+character = Player(race=race_choice, s_race=s_race_choice, clss=clss_choice)
+character.choose_abl()
+character.my_abl()
+character.my_atr()
